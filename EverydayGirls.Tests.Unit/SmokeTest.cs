e33adf23a7e513acf4909e5a56e9184c5847c9a7
amikawi <nekkoriv@gmail.com>
2026-01-16 23:52:35 -0500

    The main project was trying to compile the unit tests. A csproj update was needed for the EverydayGirlsCompanionCollector project.

---
+++ EverydayGirls.Tests.Unit/SmokeTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace EverydayGirls.Tests.Unit;
+
+public class SmokeTest
+{
+    [Fact]
+    public void Environment_Is_Configured_Correctly()
+    {
+        true.Should().BeTrue();
+    }
+}
